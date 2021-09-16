1f3b7f2f41711c70502d7cfd99915c157450ffd9
Igor Lima Charles <igorlc2004@gmail.com>
2021-09-16 10:14:05 -0300

    Quase-Finalizada

--- react/site/src/pages/devstore/index.js
+++ react/site/src/pages/devstore/index.js
@@ -4,8 +4,123 @@ import Menu from '../../components/menu'
 
 import { Container, Conteudo } from './styled'
 
+import { useState, useEffect, React, useRef } from 'react'
+
+import { confirmAlert } from 'react-confirm-alert';
+import 'react-confirm-alert/src/react-confirm-alert.css';
+
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+import LoadingBar from 'react-top-loading-bar'
+
+import Api from '../../service/api.js'
+const api = new Api();
+
 
 export default function Index() {
+
+    const [produto, setProduto] = useState([]);
+    const [nome, setNome] = useState('');
+    const [categoria, setCategoria] = useState('');
+    const [avaliacao, setAvaliacao] = useState('');
+    const [precoDe, setPrecoDe] = useState('');
+    const [precoPor, setPrecoPor] = useState('');
+    const [estoque, setEstoque] = useState('');
+    const [imagem, setImagem] = useState('');
+    const [descricao, setDescricao] = useState('');
+    const [idAlterando, setIdAlterando] = useState(0)
+    const loading = useRef();
+
+    async function listar () {
+        loading.current.continuousStart();
+        let r = await api.listar();
+        setProduto(r);
+        loading.current.complete();
+    }
+
+    useEffect (() => {
+        listar();
+    }, []);
+
+    async function inserir () {
+        loading.current.continuousStart();
+
+        if(nome !== ('') && categoria !== ('') && avaliacao !== (isNaN) && avaliacao !==('') 
+           && precoDe !== ('') && precoDe !== (isNaN) && precoPor !== ('') && precoPor !== (isNaN) 
+           && estoque !== (isNaN) && imagem !== ('') && descricao !== ('')){
+               if(idAlterando === 0) {
+                    let r = await api.inserir(nome, categoria, precoDe, precoPor, avaliacao, descricao, estoque, imagem);
+                    if(r.erro)
+                        toast.dark(r.erro)
+                    else
+                        toast.dark('Produto cadastrado com sucesso!')
+               }
+           } else {
+            let r = await api.alterando( idAlterando, nome, categoria, precoDe, precoPor, avaliacao, descricao, estoque, imagem);
+            if (r.erro)
+            toast.dark(r.erro)
+            else
+                toast.dark('Produto alterado com sucesso!')
+           }
+           listar();
+           limparCampos();
+    }
+}
+
+
+    function limparCampos () {
+        setNome('')
+        setCategoria('')
+        setPrecoDe('')
+        setPrecoPor('')
+        setAvaliacao('')
+        setDescricao('')
+        setEstoque('')
+        setImg('')
+        setIdAlterando(0);
+    }
+
+    async function remover (id) {
+        loading.current.continuousStart();
+
+        confirmAlert({
+            title: 'Remover Produto',
+            message: `Tem certeza que deseja remover o produto ${id}?`,
+            buttons: [
+                {
+                    label: 'Sim, remover',
+                    onClick: async() => {
+                        let r = await api.remover(id)
+                        if(r.erro) {
+                            toast.dark(r.erro)
+                        } else {
+                            toast.dark('Produto removido com sucesso!')
+                        }
+                        listar();
+                    }
+                },
+                {
+                    label: 'Não, Cancelar!'}
+            ]
+        })
+        listar();
+
+        loading.current.complete()
+    }
+
+    async function editar (item) {
+        setNome(item.nm_produto);
+        setCategoria(item.ds_categoria);
+        setPrecoDe(item.vl_preco_de);
+        setPrecoPor(item.vl_preco_por);
+        setAvaliacao(item.vl_avaliacao);
+        setDesc(item.ds_produto);
+        setEstoque(item.qtd_estoque);
+        setImg(item.img_produto);
+        setIdAlterando(item.id_produto);
+    }
+
     return (
         <Container>
             <Menu />
@@ -141,4 +256,3 @@ export default function Index() {
             </Conteudo>
         </Container>
     )
-}
